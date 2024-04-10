c0ab83394fcb679229844cd5c0241f5a7076753d
Thomas Manley <tmanley1985@gmail.com>
2024-04-10 15:19:02 -0400

    Adding test for multiple rules

--- external-inventory-reorder-dsl/__tests__/lexer.test.js
+++ external-inventory-reorder-dsl/__tests__/lexer.test.js
@@ -30,4 +30,48 @@ test("it should correctly lex the dsl", () => {
   expect(tokens).toEqual(expectedTokens)
 })
 
-test("it should parse multiple rules in the same file", () => {})
+test("it should parse multiple rules in the same file", () => {
+  const inputText = `
+    ReorderRule for: SKU 12345
+    When: StockLevel < MinStockLevel
+    OrderQuantity: 3 weeks sales velocity + 15%
+
+    Description: Ignored content - Some long description that may be a lot of text and contains numbers such as 5 and operators like *.
+
+    ReorderRule for: SKU 12346
+    When: StockLevel < MinStockLevel
+    OrderQuantity: 3 weeks sales velocity + 15%
+
+    Description: Ignored content - Some long description that may be a lot of text and contains numbers such as 5 and operators like *.
+    `
+
+  const expectedTokens = [
+    { type: "KEYWORD", value: "ReorderRule for:" },
+    { type: "SKU", value: "SKU 12345" },
+    { type: "KEYWORD", value: "When:" },
+    { type: "KEYWORD", value: "StockLevel" },
+    { type: "COMPARISON_OPERATOR", value: "<" },
+    { type: "KEYWORD", value: "MinStockLevel" },
+    { type: "KEYWORD", value: "OrderQuantity:" },
+    { type: "NUMERIC_LITERAL", value: "3" },
+    { type: "VELOCITY_METRIC", value: "weeks sales velocity" },
+    { type: "ARITHMETIC_OPERATOR", value: "+" },
+    { type: "NUMERIC_LITERAL", value: "15%" },
+    { type: "KEYWORD", value: "ReorderRule for:" },
+    { type: "SKU", value: "SKU 12346" },
+    { type: "KEYWORD", value: "When:" },
+    { type: "KEYWORD", value: "StockLevel" },
+    { type: "COMPARISON_OPERATOR", value: "<" },
+    { type: "KEYWORD", value: "MinStockLevel" },
+    { type: "KEYWORD", value: "OrderQuantity:" },
+    { type: "NUMERIC_LITERAL", value: "3" },
+    { type: "VELOCITY_METRIC", value: "weeks sales velocity" },
+    { type: "ARITHMETIC_OPERATOR", value: "+" },
+    { type: "NUMERIC_LITERAL", value: "15%" },
+    { type: "EOF", value: "<EOF>" },
+  ]
+
+  const tokens = new Lexer(inputText).lex()
+
+  expect(tokens).toEqual(expectedTokens)
+})

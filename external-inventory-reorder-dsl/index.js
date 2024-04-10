781485a41951bb3c7c6cb7172e4579b859a79230
Thomas Manley <tmanley1985@gmail.com>
2024-04-10 15:30:03 -0400

    Adding basic test for parser

--- external-inventory-reorder-dsl/index.js
+++ external-inventory-reorder-dsl/index.js
@@ -7,6 +7,12 @@ When: StockLevel < MinStockLevel
 OrderQuantity: 3 weeks sales velocity + 15%
 
 Description: Ignored content - Some long description that may be a lot of text and contains numbers such as 5 and operators like *.
+
+ReorderRule for: SKU 12346
+When: StockLevel < MinStockLevel
+OrderQuantity: 3 weeks sales velocity + 15%
+
+Description: Ignored content - Some long description that may be a lot of text and contains numbers such as 5 and operators like *.
 `
 
 const tokens = new Lexer(inputText).lex()
